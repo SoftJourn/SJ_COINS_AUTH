97a3681de99b46eab2511cae09f4ffdc262bfc18
vromanchuk <vromanchuk@softjourn.com>
2016-11-07 13:24:08 +0200

    Updating deprecated classes

--- src/main/java/com/softjourn/coin/auth/Auth.java
+++ src/main/java/com/softjourn/coin/auth/Auth.java
@@ -5,8 +5,10 @@ import com.softjourn.coin.auth.config.LdapConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.orm.jpa.EntityScan;
+
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
