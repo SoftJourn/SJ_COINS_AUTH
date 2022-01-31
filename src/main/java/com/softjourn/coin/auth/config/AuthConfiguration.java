72db9309b0e53f073411590c8e34fe2793149f54
vzaichuk <vzaichuk@softjourn.com>
2022-01-31 14:20:39 +0200

    Move jwt token converter to separate configuration class.

--- src/main/java/com/softjourn/coin/auth/config/AuthConfiguration.java
+++ src/main/java/com/softjourn/coin/auth/config/AuthConfiguration.java
@@ -1,7 +1,5 @@
 package com.softjourn.coin.auth.config;
 
-import java.net.MalformedURLException;
-import java.security.KeyPair;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +7,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.UrlResource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,7 +26,6 @@ import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 public class AuthConfiguration {
@@ -45,20 +41,6 @@ public class AuthConfiguration {
     private final UserDetailsService userDetailsService;
     private final JwtAccessTokenConverter jwtAccessTokenConverter;
     private final ClientDetailsService clientDetailsService;
-    private final ApplicationProperties applicationProperties;
-
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() throws MalformedURLException {
-      JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-      KeyPair keyPair = new KeyStoreKeyFactory(
-          new UrlResource("file:" + applicationProperties.getAuth().getKeyFileName()),
-          applicationProperties.getAuth().getKeyStorePass().toCharArray())
-          .getKeyPair(
-              applicationProperties.getAuth().getKeyAlias(),
-              applicationProperties.getAuth().getKeyMasterPass().toCharArray());
-      converter.setKeyPair(keyPair);
-      return converter;
-    }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
